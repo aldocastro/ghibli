290a9d745f46773eed5ac7d2dcaa262434a215ec
Aldo Castro <aldo.castro21@gmail.com>
2025-11-29 17:13:51 +0100

    Adding FilmRow to project

---
+++ Ghibli/Components/FilmRow.swift
@@ -0,0 +1,43 @@
+//
+//  FilmRow.swift
+//  Ghibli
+//
+//  Created by Aldo Castro on 29.11.25.
+//
+
+import SwiftUI
+
+struct FilmRow: View {
+    let film: Film
+
+    var body: some View {
+        HStack(alignment: .top) {
+            AsyncImage(url: film.image) { phase in
+                switch phase {
+                case .empty:
+                    ProgressView()
+                case .success(let image):
+                    image.resizable()
+                case .failure(let error):
+                    ContentUnavailableView {
+                        Label("Error al cargar la imagen", systemImage: "photo")
+                    } description: {
+                        Text(error.localizedDescription)
+                    }
+                @unknown default:
+                    EmptyView()
+                }
+            }
+            .frame(width: 80, height: 120)
+            VStack(alignment: .leading) {
+                Text("Título: \(film.title)")
+                Text("Director: \(film.director)")
+                Text("Puntuación: \(film.scorePercentage)")
+            }
+        }
+    }
+}
+
+#Preview {
+    FilmRow(film: .sample)
+}
