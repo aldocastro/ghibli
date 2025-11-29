f452dc60fb48b7923c7513fb606e9692918ca2ac
Aldo Castro <aldo.castro21@gmail.com>
2025-11-29 11:26:47 +0100

    Adding LocalizedError protocol to NetworkError type

--- Ghibli/Interface/NetworkError.swift
+++ Ghibli/Interface/NetworkError.swift
@@ -7,9 +7,25 @@
 
 import Foundation
 
-enum NetworkError: Error {
+enum NetworkError: LocalizedError {
     case general(Error)
     case nonHTTP
     case json(Error)
     case status(Int)
+    case dataNotValid
+    
+    var errorDescription: String? {
+        return switch self {
+        case .general(let error):
+            "\(error)"
+        case .nonHTTP:
+            "The response was not an HTTP response."
+        case .json(let error):
+            "Failed upon JSON serialization: \(error)"
+        case .status(let code):
+            "Invalid response status code: \(code)."
+        case .dataNotValid:
+            "Invalid response data."
+        }
+    }
 }
