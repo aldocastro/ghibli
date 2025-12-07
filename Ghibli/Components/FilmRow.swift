e008e90a98c6a2e5a6c34398db911c71d27a32f7
Aldo Castro <aldo.castro21@gmail.com>
2025-12-07 18:55:59 +0100

    Styling FilmRow

--- Ghibli/Components/FilmRow.swift
+++ Ghibli/Components/FilmRow.swift
@@ -12,41 +12,55 @@ struct FilmRow: View {
     let isFavorite: Bool
 
     var body: some View {
-        HStack(alignment: .top) {
-            AsyncImage(url: film.image) { phase in
-                switch phase {
-                case .empty:
-                    ProgressView()
-                case .success(let image):
-                    image
-                        .resizable()
-                        .scaledToFill()
-                        .clipShape(RoundedRectangle(cornerRadius: 8))
-                case .failure(let error):
-                    ContentUnavailableView {
-                        Label("Error al cargar la imagen", systemImage: "photo")
-                    } description: {
-                        Text(error.localizedDescription)
+        HStack {
+            HStack(alignment: .top) {
+                AsyncImage(url: film.image) { phase in
+                    switch phase {
+                    case .empty:
+                        ProgressView()
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                    case .failure(let error):
+                        ContentUnavailableView {
+                            Label(
+                                "Error al cargar la imagen",
+                                systemImage: "photo"
+                            )
+                        } description: {
+                            Text(error.localizedDescription)
+                        }
+                    @unknown default:
+                        EmptyView()
                     }
-                @unknown default:
-                    EmptyView()
+                }
+                .frame(width: 80, height: 120)
+
+                VStack(alignment: .leading) {
+                    Text("\(film.title)")
+                    Text("\(film.director)")
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                    HStack {
+                        Image(systemName: "star.fill")
+                            .foregroundColor(.yellow)
+                        Text("\(film.scorePercentage)")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
                 }
             }
-            .frame(width: 80, height: 120)
-            VStack(alignment: .leading) {
-                Text("Título: \(film.title)")
-                Text("Director: \(film.director)")
-                Text("Puntuación: \(film.scorePercentage)")
-                Label("Añadido a favoritos", systemImage: "heart.fill")
-                    .labelStyle(.iconOnly)
-                    .foregroundStyle(.red)
-                    .foregroundStyle(.black)
-                    .opacity(isFavorite ? 1.0 : 0.0)
-            }
+            Spacer()
+            Label("Añadido a favoritos", systemImage: "heart.fill")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(.red)
+                .opacity(isFavorite ? 1.0 : 0.0)
         }
     }
 }
 
 #Preview {
-    FilmRow(film: .sample, isFavorite: false)
+    FilmRow(film: .sample, isFavorite: true)
 }
